c1416b0ab4e32b8a3a2efcb334f0eff3ed89b853
rDelgC <rdelgado.dev@gmail.com>
2022-03-18 14:25:46 -0500

    Versioning initialization

---
+++ src/test/java/com/boot/bansis/api/gateway/BansisApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.bansis.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BansisApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
